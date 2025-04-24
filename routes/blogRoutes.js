dd9d07ad94a8fd0f5fe030a2334758854fc1e638
RUKUNDO MICHEL <160488320+rukundomichel@users.noreply.github.com>
2025-04-24 10:10:36 +0200

    added get all blogs route

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,13 +1,17 @@
 import e from "express";
+import {Blog} from '../models/blogsModel.js'
 
 const router = e.Router();
 
 //Get all blogs
 router.get('/', async(request, response) => {
     try{
-        
+        const blogs = await Blog.find();
+
+        response.status(200).json({data: blogs})
     }catch(err){
         console.log(err);
+        response.status(500).json({success: false, message: 'System Error!'})
         
     }
 })
